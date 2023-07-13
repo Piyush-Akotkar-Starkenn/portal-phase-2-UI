371b643da5eb4e8c620fdb348b91cd8a5fc1fa69
sapnaRL <sapna@starkenn.com>
2023-07-13 13:30:29 +0530

    vehicle grid updated

--- src/views/AllRoutes.jsx
+++ src/views/AllRoutes.jsx
@@ -4,8 +4,6 @@ import SignIn from "./auth/SignIn";
 import Profile from "./auth/Profile";
 import CustomerLayout from "layouts/customer";
 import AdminLayout from "layouts/admin";
-import AddVehicle from "./admin/vehicles/AddVehicle";
-import EditCustomer from "./admin/customers/components/EditCustomer";
 
 const AllRoutes = () => {
   return (
@@ -13,10 +11,8 @@ const AllRoutes = () => {
       <Route path="signin" element={<SignIn />} />
       <Route path="profile" element={<Profile />} />
       <Route path="admin/*" element={<AdminLayout />} />
-      <Route path="admin/vehicles/addvehicle" element={<AddVehicle />} />
       <Route path="customer/*" element={<CustomerLayout />} />
       <Route path="/" element={<Navigate to="/customer" replace />} />
-      <Route path="edit-customer" element={<EditCustomer />} />
     </Routes>
   );
 };
